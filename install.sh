7b34cf2864152e0a273c606c0095f57a105ecc4b
Daniel Meißner <daniel@3st.be>
2013-01-25 18:39:41 +0100

    Refactured install script to accept parameter to install or remove dotfiles.

--- install.sh
+++ install.sh
@@ -9,37 +9,70 @@ fi
 PWD=`pwd`
 SCRIPT_DIR="${PWD}/${DIR_NAME}"
 
-# if dotfiles not exits, link them to repositorie
-for FILE in `ls -1a \
-                -I $0 \
-                -I bin \
-                -I README.md \
-                -I . -I .. \
-                -I .git -I .gitignore \
-                -I install.rb -I install.sh \
-                $SCRIPT_DIR`; do
+DOTFILES=`ls -1a \
+              -I $0 \
+              -I bin \
+              -I README.md \
+              -I . -I .. \
+              -I .git -I .gitignore \
+              -I install.rb -I install.sh \
+              $SCRIPT_DIR`
 
-  if [ ! -e $HOME/$FILE ]; then
-    ln -s $SCRIPT/$FILE $HOME/$FILE
-  fi
+link_files() {
+  # if dotfiles not exists, link them to repositorie
+  for FILE in $DOTFILES; do
+    if [ ! -e $HOME/$FILE ]; then
+      ln -s $SCRIPT/$FILE $HOME/$FILE
+    fi
 
-  echo `ls -lgo $HOME/$FILE`
-done
+    echo `ls -lgo $HOME/$FILE`
+  done
+}
 
-# create ~/.zsh directory if not exist
-for DIRECTORY in "${HOME}/.zsh"; do
-  if [ ! -e $DIRECTORY ]; then
-    mkdir $DIRECTORY
-    echo $DIRECTORY created
-  fi
-done
+create_directories() {
+  # create required directories if not exist
+  for DIRECTORY in "${HOME}/.zsh"; do
+    if [ ! -e $DIRECTORY ]; then
+      mkdir $DIRECTORY
+      echo $DIRECTORY created
+    fi
+  done
+}
 
-# create empty file ~/.portknocking if not exist
-for FILE in "$HOME/.portknocking"; do
-  if [ ! -e $FILE ]; then
-    touch $FILE
-    echo $FILE created
-  fi
-done
+create_files() {
+  # create empty files
+  for FILE in "$HOME/.portknocking"; do
+    if [ ! -e $FILE ]; then
+      touch $FILE
+      echo $FILE created
+    fi
+  done
+}
+
+# remove dotfiles
+remove_files() {
+  echo "Nothing removed."
+}
+
+remove_directories() {
+  echo "Nothing removed."
+}
+
+case $1 in
+  install )
+    link_files
+    create_files
+    create_directories
+    ;;
+
+  remove|uninstall )
+    remove_files
+    remove_directories
+    ;;
+  * )
+    echo "Usage: ${PWD}/${0} <install|remove>"
+    exit 1
+    ;;
+esac
 
 exit 0
